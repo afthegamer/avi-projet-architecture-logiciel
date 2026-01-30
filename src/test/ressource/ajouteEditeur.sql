3802a745e8da9076c2327fa2c471457a41742a6a
Brad Dos Santos Patatas <brad.dossantospatatas@acedigitale.com>
2026-01-30 10:56:43 +0100

    Remove duplicate test ressource directory

--- src/test/ressource/ajouteEditeur.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO editeur(id, nom, logo) VALUES (1, 'Amazon', 'logo.png');
-INSERT INTO editeur(id, nom, logo) VALUES (2, 'Microsoft', 'logo.png');
-INSERT INTO editeur(id, nom, logo) VALUES (3, 'Google', 'logo.jpg');
